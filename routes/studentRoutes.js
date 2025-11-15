295dae66ee9cae87605a6b6c6cfc3d276e9bfc3f
adstahly <adstahly@gmail.com>
2025-11-15 15:32:04 -0600

    Added Routes

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -5,6 +5,14 @@ const studentController = require('../controllers/studentController');
 
 router.use(methodOverride('_method'));
 
+router.get('/', (req, res) => res.redirect('/students'));
+
 router.get('/students', studentController.listStudents);
+router.get('/students/new', studentController.newStudentForm);
+router.post('/students', studentController.createStudent);
+router.get('/students/:id/edit', studentController.editStudentForm);
+router.get('/students/:id', studentController.showStudent);
+router.put('/students/:id', studentController.updateStudent);
+router.delete('/students/:id', studentController.deleteStudent);
 
 module.exports = router;
